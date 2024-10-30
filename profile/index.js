2df71269e4ad7ad7f470bb1f491ab077f404789e
James Haley <conbongu5@outlook.com>
2024-10-30 14:00:19 +0700

    Create index.js

---
+++ profile/index.js
@@ -0,0 +1,7 @@
+// Abgerny Game Initialization
+import { initializeGame } from './gameLogic';
+
+document.addEventListener('DOMContentLoaded', () => {
+    initializeGame();
+    console.log('Welcome to Abgerny!');
+});
